dc01374db537cc14712b29b0f0f2db4bca6453ca
cuidong233 <cuidong111@gmail.com>
2026-06-23 16:22:29 +0800

    Split production chunks

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,20 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/bazi-classified-site/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("/node_modules/")) {
+            return "vendor";
+          }
+
+          if (id.endsWith("/src/data/siteData.js")) {
+            return "site-data";
+          }
+        }
+      }
+    }
+  },
   plugins: [react()],
 });
